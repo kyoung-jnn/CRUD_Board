73ab424552c5a3fd74ec9735a824b83b81433735
Rohkyoungjin <rudwls468@gmail.com>
2020-03-08 19:32:20 +0900

    Create 구현
    UI는 더 구현해야함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Contents(props){
+  const [contents,setContents] = useState({
+    title: '',
+    desc: ''
+  });
+
+  function handleChange(e){
+    setContents({
+      ...contents,
+      [e.target.name]:e.target.value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target);
+    props.onSave(contents);
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="title" name="title" onChange={handleChange}></input>
+        <input placeholder="desc" name="desc" onChange={handleChange}></input>
+        <input type="submit" value="ADD"></input>
+      </form>
+    </div>
+  );
+}
+
+function Template(){
+  var [userData,setData] = useState({});
+
+  function onSaveContents(data){
+    console.log(data);
+    setData(data);
+  }
+
+  return(
+    <body>
+      <section>
+        <header>User status</header>
+        <nav>menu</nav>
+        <article>
+        </article>
+        <Contents onSave={onSaveContents}></Contents>
+      </section>
+    </body>
+  );
+}
+
 function App() {
+  var [userData,setData] = useState({
+    boards:
+    [
+      {
+        title:'Hello',
+        desc:'HI',
+        writer:'Jin'
+      },
+      {
+        title:'Good bye',
+        desc:'GG',
+        writer:'Roh'
+      }
+    ]
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Template></Template>
     </div>
   );
 }
