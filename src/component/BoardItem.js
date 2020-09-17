e7e28ea8dfdc0d6a16d01efaf54a5890326fbae7
Rohkyoungjin <rudwls468@gmail.com>
2020-09-18 01:44:46 +0900

    save 구현중

--- src/component/BoardItem.js
+++ src/component/BoardItem.js
@@ -3,32 +3,31 @@ import { board_read, board_remove } from "./App_reducer";
 import { connect } from "react-redux";
 
 function BoardItem(props) {
-  const handleUpdateForm = (props, brdnum) => {
+  
+  const handleUpdateForm = (brdnum) => {
     props.dispatch(board_read(brdnum));
   };
 
   const row = props.row;
   return (
-    <tbody>
-      <tr>
-        <td>{row.brdnum}</td>
-        <td>
-          <a onClick={() => handleUpdateForm(props, row.brdnum)}>{row.title}</a>
-        </td>
-        <td>{row.writer}</td>
-        <td>{row.date.toLocaleDateString("kr-KR")}</td>
-        <td>
-          <button
-            className="delBtn"
-            onClick={() => {
-              props.dispatch(board_remove(row.brdnum));
-            }}
-          >
-            X
-          </button>
-        </td>
-      </tr>
-    </tbody>
+    <tr>
+      <td>{row.brdnum}</td>
+      <td>
+        <a onClick={() => handleUpdateForm(row.brdnum)}>{row.title}</a>
+      </td>
+      <td>{row.writer}</td>
+      <td>{row.date.toLocaleDateString("kr-KR")}</td>
+      <td>
+        <button
+          className="delBtn"
+          onClick={() => {
+            props.dispatch(board_remove(row.brdnum));
+          }}
+        >
+          X
+        </button>
+      </td>
+    </tr>
   );
 }
 
