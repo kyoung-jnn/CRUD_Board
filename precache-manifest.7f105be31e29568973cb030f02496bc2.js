037aa9532ed5667fa30fa399a0e2f933bc60bc24
Rohkyoungjin <rudwls468@gmail.com>
2021-05-22 19:53:17 +0900

    Updates

---
+++ precache-manifest.7f105be31e29568973cb030f02496bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff3aca3ce11ddc015a9ccdb19a59f70",
+    "url": "/kyoung-jnn/CRUD_Board/index.html"
+  },
+  {
+    "revision": "dd2f08eddcd2bb9ade1b",
+    "url": "/kyoung-jnn/CRUD_Board/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "a1f5031d081105bdaad0",
+    "url": "/kyoung-jnn/CRUD_Board/static/css/main.be0b2c1a.chunk.css"
+  },
+  {
+    "revision": "dd2f08eddcd2bb9ade1b",
+    "url": "/kyoung-jnn/CRUD_Board/static/js/2.33433111.chunk.js"
+  },
+  {
+    "revision": "17449cf5da0cd0edea3cc354e55c1054",
+    "url": "/kyoung-jnn/CRUD_Board/static/js/2.33433111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f5031d081105bdaad0",
+    "url": "/kyoung-jnn/CRUD_Board/static/js/main.4437b805.chunk.js"
+  },
+  {
+    "revision": "2b745ad0c129bbc15990",
+    "url": "/kyoung-jnn/CRUD_Board/static/js/runtime-main.defc5097.js"
+  }
+]);
